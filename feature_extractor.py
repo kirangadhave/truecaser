7099a40e39c30d6b7b5e4fbc5a4520f8ce791498
kbg <kirangadhave2@gmail.com>
2017-11-10 01:29:15 -0700

    Update

--- feature_extractor.py
+++ feature_extractor.py
@@ -1,16 +1,26 @@
 import pandas as pd
 import numpy as np
 
+
 class Word:
     def __init__(self, word, position, pos = "UNK"):
         self.actual_word = word
         self.word_position = position
         self.pos = pos
 
-    def print(self):
-        print(self.actual_word, self.word_position, self.pos)
+    def print(self, pad_length=16):
+        print(str("{:<" + str(pad_length) + "}").format("Actual Word: "), self.actual_word)
+        print(str("{:<" + str(pad_length) + "}").format("Position: "), self.word_position)
+        print(str("{:<" + str(pad_length) + "}").format("Part of Speech: "), self.pos)
+
 
 def word_featurizer(word):
     word.print()
 
-word_featurizer(Word("test_word", 0))
+
+def main():
+    word_featurizer(Word("testing", 0))
+
+
+if __name__ == "__main__":
+    main()
