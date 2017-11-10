34b6a7464068c17c166623768aa96ea2988e9f91
kbg <kirangadhave2@gmail.com>
2017-11-10 02:31:14 -0700

    Update

--- feature_extractor.py
+++ feature_extractor.py
@@ -7,20 +7,19 @@ class Word:
         self.actual_word = word
         self.word_position = position
         self.pos = pos
+        self.first_capital = False
+        if self.actual_word[0].isupper():
+            self.first_capital = True
 
     def print(self, pad_length=16):
         print(str("{:<" + str(pad_length) + "}").format("Actual Word: "), self.actual_word)
         print(str("{:<" + str(pad_length) + "}").format("Position: "), self.word_position)
         print(str("{:<" + str(pad_length) + "}").format("Part of Speech: "), self.pos)
+        print(str("{:<" + str(pad_length) + "}").format("Is Capital: "), str(self.first_capital))
 
 
 def word_featurizer(word):
     word.print()
 
 
-def main():
-    word_featurizer(Word("testing", 0))
-
-
-if __name__ == "__main__":
-    main()
+word_featurizer(Word("Testing", 0))
