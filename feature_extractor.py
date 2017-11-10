d0e739a51a9a1f2ddb74a3d0f88222995566ec5e
KIRAN GADHAVE <u1143683@lab2-2.eng.utah.edu>
2017-11-10 14:38:37 -0700

    Update

--- feature_extractor.py
+++ feature_extractor.py
@@ -3,6 +3,7 @@ from nltk import word_tokenize, sent_tokenize, pos_tag
 
 class Word:
     def __init__(self, word, position, pos = "UNK"):
+        self.word = word
         self.is_first_word = 0
         self.is_proper_noun = 0
         self.is_capital = 0
@@ -19,17 +20,11 @@ class Word:
     def print(self):
         print(self.vector)
 
-
-def sentence_vectorize(sentence):
-    for word in sent_tokenize(sentence):
-        print(word)
-
-
 def vectorize_text(text):
     vectors = []
     tagged_words = []
 
     for sentence in sent_tokenize(text):
         for i,word in enumerate(pos_tag(word_tokenize(sentence))):
-            vectors.append(Word(word[0], i, word[1]).vector)
-    return vectors
+            vectors.append(Word(word[0], i, word[1]))
+    return [w.word for w in vectors],[w.vector for w in vectors]
--- true_case.py
+++ true_case.py
@@ -5,32 +5,25 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn import svm
 from sklearn.metrics import accuracy_score
 
-train_text = '''
-While drawing in class to avoid listening to a story from his painfully boring teacher at Stagwood School, 12-year old Cal sees a frog staring at him through the window. Odder than that is the fact that this frog happens to be wearing glasses. 
 
-Cal and his best friend, the tactless but loyal Soy, learn that the frog (who prefers the name Deli) has sought them out for a reason. When a school administrator named Ream reveals himself to be a dragon, the boys discover that fairytales are real, and that there is magic afoot in Stagwood. With Ream on their tail, the trio must unearth a powerful tool protected by riddles and rile (the magic that fuels nightmares) to save the fate of all fairytales past. Their only means on conveyance, Cal's now-flying bed, takes them on a journey beyond the home of the fairies (a cloud floating somewhere over Iceland) to set things right. But, before Cal can defeat Ream and his kidnapped army of fairies, he has to deal with Soy's knack for arguing with magical creatures, discover the truth about Deli's identity, and earn his place as the hero of the story. 
- 
 
-The Guardians of Lore is a middle grade novel that centers around two life-long friends, infusing humor and fantasy-based riddles into a modern fairytale.
-'''
 
-test_text = '''
-In this article, we have learned how to model the decision tree algorithm in Python using the Python machine learning library scikit-learn. In the process, we learned how to split the data into train and test dataset. To model decision tree classifier we used the information gain, and gini index split criteria. In the end, we calucalte the accuracy of these two decision tree models.'''
-
-train_vectors = pd.DataFrame(vectorize_text(train_text), columns=["first_word", "proper_noun", "label"])
+# Create training vectors
+train_vectors = pd.DataFrame(vectorize_text(test_text)[1], columns=["first_word", "proper_noun", "label"])
+print(train_vectors)
 X_train = train_vectors.iloc[:, 0:2].values
 Y_train = train_vectors.iloc[:, 2].values
 
-test_vectors = pd.DataFrame(vectorize_text(test_text), columns=["first_word", "proper_noun", "label"])
+# Create test vectors
+test_vectors = pd.DataFrame(vectorize_text(test_text)[1], columns=["first_word", "proper_noun", "label"])
 X_test = test_vectors.iloc[:, 0:2].values
 Y_test = test_vectors.iloc[:, 2].values
-test_vectors = pd.DataFrame(vectorize_text(test_text.lower()), columns=["first_word", "proper_noun", "label"])
+test_vectors = pd.DataFrame(vectorize_text(test_text.lower())[1], columns=["first_word", "proper_noun", "label"])
 X_test = test_vectors.iloc[:, 0:2].values
 
 
+# Train Support Vector Machine
 clf = svm.SVC(kernel='linear', gamma = 10)
 clf.fit(X_train, Y_train)
-
 preds = clf.predict(X_test)
-
 print(accuracy_score(Y_test, preds)*100)
