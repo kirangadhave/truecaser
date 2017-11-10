6c11dcef83b24c66aa3344e24f2b2c5a2534602c
kbg <kirangadhave2@gmail.com>
2017-11-10 02:53:15 -0700

    Update

--- feature_extractor.py
+++ feature_extractor.py
@@ -4,22 +4,25 @@ import numpy as np
 
 class Word:
     def __init__(self, word, position, pos = "UNK"):
-        self.actual_word = word
-        self.word_position = position
-        self.pos = pos
-        self.first_capital = False
-        if self.actual_word[0].isupper():
-            self.first_capital = True
+        self.is_first_word = 0
+        self.is_proper_noun = 0
+        self.is_capital = 0
+
+        if position == 0:
+            self.is_first_word = 1
+        if "NNP" in pos:
+            self.is_proper_noun = 1
+        if word[0].isupper():
+            self.is_capital = 1
+
+        self.vector = [self.is_first_word, self.is_proper_noun, self.is_capital]
 
     def print(self, pad_length=16):
         print(str("{:<" + str(pad_length) + "}").format("Actual Word: "), self.actual_word)
         print(str("{:<" + str(pad_length) + "}").format("Position: "), self.word_position)
         print(str("{:<" + str(pad_length) + "}").format("Part of Speech: "), self.pos)
         print(str("{:<" + str(pad_length) + "}").format("Is Capital: "), str(self.first_capital))
-
-
-def word_featurizer(word):
-    word.print()
-
+        print()
+        print(self.vector)
 
 word_featurizer(Word("Testing", 0))
